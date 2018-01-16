685d23b78d93144e1152bb69b7e9bfb2b0857776
GabrielAguiar98 <Gabrielaguiar2016@gmail.com>
2018-01-15 21:23:49 -0800

    test1 work

---
+++ planets_sort.c
@@ -0,0 +1,15 @@
+// Project 8 Problem 2 Planet Sort
+// CS 135 due 12/5/17
+// Gabriel Aguiar
+
+#include <stdio.h>
+#include <string.h>
+int string_compare(char str1[], char str2[]);
+int main(int argc, char *argv[]){
+	
+	return 0;	
+}
+
+int string_compare(char str1[], char str2[]){
+	
+}
